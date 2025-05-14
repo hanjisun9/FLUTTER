d091abbc262c1a3ebf99a1d7c0f89a49abca6fdb
jujuuu <azizahnf85@gmail.com>
2025-05-14 12:06:00 +0700

    profile_screen.dart

---
+++ profile_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text('Ini Halaman Profile', style: TextStyle(fontSize: 24)));
+  }
+}
