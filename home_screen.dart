661596a9af322beff21b11d525343ccf50e73bca
jujuuu <azizahnf85@gmail.com>
2025-05-14 12:04:49 +0700

    home_screen.dart

---
+++ home_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text('Ini Halaman Home', style: TextStyle(fontSize: 24)));
+  }
+}
